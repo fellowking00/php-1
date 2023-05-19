78ef60ebad38e6b104f95100ae8367ed526192bc
PROBE <86278730+fellowking00@users.noreply.github.com>
2023-05-19 06:25:06 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Messages;
 
-require __DIR__.'/../vendor/autoload.php';
+echo "Working shit";
 
-$messages = new Messages();
-
-printf("<h1>%s</h1>\n", $messages->title());
-printf("<p>%s</p>\n", $messages->message());
+?>
